ef0c7f1d4136f55a8ec7054a1a300d72f0d7550c
Joseph M <jmedina1985@hotmail.com>
2021-02-08 21:03:41 -0800

    Hero image - 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <Router>
       <GlobalStyle></GlobalStyle>
       <Navbar />
+      <Hero />
     </Router>
   );
 }
