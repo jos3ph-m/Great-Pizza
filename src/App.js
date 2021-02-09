e64528a0f4f46e366589afd82dfe8056765cdcdb
Joseph M <jmedina1985@hotmail.com>
2021-02-08 20:51:21 -0800

    Global styles - 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle></GlobalStyle>
       <Navbar />
     </Router>
   );
