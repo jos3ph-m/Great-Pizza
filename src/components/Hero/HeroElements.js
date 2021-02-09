7f098f97a12b6012301a7681a81a463a3590a7f3
Joseph M <jmedina1985@hotmail.com>
2021-02-08 23:13:05 -0800

    Hero image - 8

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -36,7 +36,7 @@ export const HeroItems = styled.div`
 `;
 
 export const HeroH1 = styled.h1`
-  font-size: clamp(2.5rem, 10vw, 5rem)
+  font-size: clamp(2.5rem, 10vw, 5rem);
   margin-bottom: 1rem;
   box-shadow: 3px 5px #e9ba23;
   letter-spacing: 3px;
