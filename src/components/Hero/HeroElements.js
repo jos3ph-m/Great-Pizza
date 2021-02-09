5971761d9694f74183a5daf2dc34cfe5a4eb0274
Joseph M <jmedina1985@hotmail.com>
2021-02-08 23:18:57 -0800

    Hero image - 9

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -41,3 +41,23 @@ export const HeroH1 = styled.h1`
   box-shadow: 3px 5px #e9ba23;
   letter-spacing: 3px;
 `;
+
+export const HeroP = styled.p`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  margin-bottom: 2rem;
+`;
+
+export const HeroBtn = styled.button`
+  font-size: 1.4 rem;
+  padding: 1rem 4rem;
+  border: none;
+  background: #e31837;
+  transition: 0.2s ease-out;
+
+  &:hover {
+    background: #ffc500;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+    color: #000;
+  }
+`;
