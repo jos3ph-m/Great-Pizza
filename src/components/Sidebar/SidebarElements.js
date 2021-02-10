fe1fe73f2413ed4e5a2e147c0188331177caa89a
Joseph M <jmedina1985@hotmail.com>
2021-02-09 21:37:57 -0800

    Sidebar - 10

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -22,3 +22,14 @@ export const SidebarContainer = styled.aside`
 export const CloseIcon = styled(FaTimes)`
   color: #000;
 `
+
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  border: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`
