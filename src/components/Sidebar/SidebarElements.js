aebf8e073ffd7849b41561640324312feeb6880b
Joseph M <jmedina1985@hotmail.com>
2021-02-09 22:01:10 -0800

    Sidebar - 12

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -39,4 +39,26 @@ export const SidebarMenu = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: repeat(3, 80px);
   text-align: center;
+
+  @media screen and (max-width: 480px) {
+    grid-template-rows: repeat(3, 60px);
+  }
+`
+
+export const SidebarLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #000;
+  cursor: pointer;
+
+  &:hover {
+    color: #e31837;
+    transition: 0.2s ease-in-out;
+  }
 `
