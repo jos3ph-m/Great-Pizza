7ec54d403f52a8be26284e36767b02b4be11eb25
Joseph M <jmedina1985@hotmail.com>
2021-02-09 21:32:47 -0800

    Sidebar - 7

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 
-export const SidebarContainer = styled.aside``;
+export const SidebarContainer = styled.aside`
+  position: fixed;
+  z-index: 999;
+  width: 350px;
+  height: 100%;
+`;
