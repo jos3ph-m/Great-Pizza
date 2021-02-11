26f51e0a78b9ae1cb6b7f26689c1b21d03213d97
Joseph M <jmedina1985@hotmail.com>
2021-02-10 23:30:46 -0800

    Products - 3

---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'The House',
+    desc: 'Marinara, Basil, Italian Sausage, Tomatoes, Olives, and Pepperoni',
+    price: '$18.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'The Island',
+    desc: 'Marinara, Basil, Italian Sausage, Tomatoes, Olives, and Pepperoni',
+    price: '$14.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'The Garden',
+    desc: 'Marinara, Basil, Italian Sausage, Tomatoes, Olives, and Pepperoni',
+    price: '$17.99',
+    button: 'Add to Cart',
+  },
+];
