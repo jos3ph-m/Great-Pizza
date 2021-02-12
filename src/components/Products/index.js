4db456e25acad6cc46108fada4df3bb794c28566
Joseph M <jmedina1985@hotmail.com>
2021-02-11 23:20:22 -0800

    Products - 9

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {
-  ProductsContainer
-}
+  ProductsContainer,
+  ProductWrapper,
+  ProductsHeading,
+  ProductTitle,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from './ProductsElements';
 
 const Products = () => {
   return (
@@ -11,7 +20,7 @@ const Products = () => {
         {data.map((product, index) => {
           return (
             <ProductCard key={index}>
-              <ProductImage src={product.img} alt={product.alt} />
+              <ProductImg src={product.img} alt={product.alt} />
               <ProductInfo>
                 <ProductTitle>{product.name}</ProductTitle>
                 <ProductDesc>{product.desc}</ProductDesc>
