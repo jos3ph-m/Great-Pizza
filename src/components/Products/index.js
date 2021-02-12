aeb722bfb44393d5dc5cc95b0108c6568c1a218f
Joseph M <jmedina1985@hotmail.com>
2021-02-11 23:18:30 -0800

    Products - 7

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -11,6 +11,9 @@ const Products = () => {
               <ProductImage src={product.img} alt={product.alt} />
               <ProductInfo>
                 <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
               </ProductInfo>
             </ProductCard>
           );
