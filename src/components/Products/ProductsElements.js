43dc10412025c655253d5ddfa062a20f49cc0165
Joseph M <jmedina1985@hotmail.com>
2021-02-11 23:26:40 -0800

    Products - 13

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -7,3 +7,29 @@ export const ProductsContainer = styled.div`
   background: #150f0f;
   color: #fff;
 `;
+
+export const ProductWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+export const ProductCard = styled.div`
+  margin: 0 2rem;
+  line-height: 2;
+  width: 300px;
+`;
+
+export const PorductImg = styled.img`
+  height: 300px;
+  min-width: 300px;
+  max-width: 100%;
+  box-shadow: 8px 8px #fdc500;
+`;
+
+export const ProductHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+`;
