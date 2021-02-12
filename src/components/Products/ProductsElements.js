9bf280e9ffb3fd005bd653fdc5ae3226a7e8dc57
Joseph M <jmedina1985@hotmail.com>
2021-02-11 23:23:51 -0800

    Products - 12

--- src/components/Products/ProductsElements.js
+++ src/components/Products/ProductsElements.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const ProductsContainer = styled.div`
   width: 100vw;
   min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: #150f0f;
+  color: #fff;
 `;
