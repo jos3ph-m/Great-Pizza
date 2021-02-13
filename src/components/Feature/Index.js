b44be9772faec1369c9797e741aabee6c4bddc62
Joseph M <jmedina1985@hotmail.com>
2021-02-12 22:32:55 -0800

    Feature - 3

--- src/components/Feature/Index.js
+++ src/components/Feature/Index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Feature = () => {
-  return <div></div>;
+  return (
+    <FeatureContainer>
+      <H1>Pizza of the Day</H1>
+      <p>Truffle alfredo sauce topped with 24 carat gold</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
 };
 
 export default Feature;
