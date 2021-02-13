dd2a01981970ef4649c2cd772940c98bc5415d9e
Joseph M <jmedina1985@hotmail.com>
2021-02-12 22:38:51 -0800

    Feature - 6

--- src/components/Feature/FeatureElements.js
+++ src/components/Feature/FeatureElements.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
 import FeaturePic from '../../images/featured3.jpg';
 
-export const FeatureContainer = styled.div``;
+export const FeatureContainer = styled.div`
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
+    url(${FeaturePic});
+`;
