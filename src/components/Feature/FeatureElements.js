b448dd2560ace887ddf1d88dd007a70f3ca871f2
Joseph M <jmedina1985@hotmail.com>
2021-02-12 22:39:34 -0800

    Feature - 7

--- src/components/Feature/FeatureElements.js
+++ src/components/Feature/FeatureElements.js
@@ -4,4 +4,10 @@ import FeaturePic from '../../images/featured3.jpg';
 export const FeatureContainer = styled.div`
   background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
     url(${FeaturePic});
+  height: 100vh;
+  max-height: 500px;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
 `;
