9bfcb7ae1f008a72728a547fc1263d05dbd43695
Joseph M <jmedina1985@hotmail.com>
2021-02-13 22:57:36 -0800

    Extended Data - 15

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Products from './components/Products';
 import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Feature/Index';
+import Footer from './components/Footer/Index';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
       <Products heading="Sweet Treats for You" data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
