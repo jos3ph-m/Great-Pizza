031437dd53ead3c084818f728008ba36dc1c5ad6
Joseph M <jmedina1985@hotmail.com>
2021-02-13 22:21:44 -0800

    Extended Data - 6

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,7 @@
 import product1 from '../../images/product-1.jpg';
 import product2 from '../../images/product-2.jpg';
 import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
 
 export const productData = [
   {
