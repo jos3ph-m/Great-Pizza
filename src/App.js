dfde4a24ecfe8764839c17f8974cea5e295e5392
Joseph M <jmedina1985@hotmail.com>
2021-02-13 22:47:08 -0800

    Extended Data - 10

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
 import Products from './components/Products';
-import { productData } from './components/Products/data';
+import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Feature/Index';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
     </Router>
   );
 }
