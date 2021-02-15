a87e7eab472f469606b3b64040cede0301b750cf
Joseph M <jmedina1985@hotmail.com>
2021-02-14 22:08:57 -0800

    finishing touches - 7

--- src/components/Feature/Index.js
+++ src/components/Feature/Index.js
@@ -5,7 +5,7 @@ const Feature = () => {
   return (
     <FeatureContainer>
       <h1>Pizza of the Day</h1>
-      <p>Truffle alfredo sauce topped with 24 carat gold</p>
+      <p>Alfredo, White Truffle, Black Olive, and Ricotta</p>
       <FeatureButton>Order Now</FeatureButton>
     </FeatureContainer>
   );
